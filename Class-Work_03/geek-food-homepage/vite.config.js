5a42cef247789d39a994f164565dab46b519fbda
buriburi-nik <niikgharat001@gmail.com>
2025-02-16 00:58:08 +0530

    Set base URL for Vite configuration to GitHub repository

--- Class-Work_03/geek-food-homepage/vite.config.js
+++ Class-Work_03/geek-food-homepage/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: 'https://github.com/buriburi-nik/React_Assignments', 
   plugins: [react()],
 })
