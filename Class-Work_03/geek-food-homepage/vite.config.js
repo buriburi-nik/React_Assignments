08a61b06d2ada218e15f9edcec72c27019b20497
buriburi-nik <niikgharat001@gmail.com>
2025-02-16 01:15:29 +0530

    Remove base URL configuration from Vite setup

--- Class-Work_03/geek-food-homepage/vite.config.js
+++ Class-Work_03/geek-food-homepage/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: 'https://github.com/buriburi-nik/React_Assignments', 
   plugins: [react()],
 })
