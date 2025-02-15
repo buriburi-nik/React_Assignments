8a59565a87ee336605f515e4a2ba449e9535b70b
buriburi-nik <niikgharat001@gmail.com>
2025-02-16 00:34:54 +0530

    Enhance responsive design for Main, Navbar, Hero, and Cards components; update styles and structure for better mobile experience

--- Class-Work_03/geek-food-homepage/src/componets/Cards/Cards.jsx
+++ Class-Work_03/geek-food-homepage/src/componets/Cards/Cards.jsx
@@ -1,54 +1,59 @@
-import"./Card.css"
+import "./Card.css";
 export default function Cards() {
-    const Data = [
+  const Data = [
     {
       id: 1,
       name: "Gladis Lemon",
       title: "Head of SEO",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet."
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet.",
+      image: "/src/assets/profile.png"
     },
     {
       id: 2,
       name: "Nikhi la",
       title: "Head of SEO",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit beatae incidunt."
-        },
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit beatae incidunt.",
+      image: "/src/assets/profile.png"
+    },
     {
       id: 3,
       name: "Gladis Lemon",
       title: "Head of SEO",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet."
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet.",
+      image:"/src/assets/profile.png"
     },
     {
       id: 4,
       name: "Nikhi la",
       title: "Head of SEO",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit beatae incidunt."
-        },
-        {
-            id: 5,      
-            name: "Gladis Lemon",
-            title: "Head of SEO",
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet."
-        },
-    
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit beatae incidunt.",
+      image: "/src/assets/profile.png"
+    },
+    {
+      id: 5,
+      name: "Gladis Lemon",
+      title: "Head of SEO",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet.",
+      image: "/src/assets/profile.png"
+    }
   ];
-  
-      return (
+
+  return (
     <section className="Cards-section">
       <div className="disp-grid">
         {Data.map((item) => (
           <div className="card" key={item.id}>
-            <p>{item.text}</p>
+            
+            
             <div className="author">
+              <img src={item.image} alt={item.name} className="profile-img" />
               <h4>{item.name}</h4>
               <span>{item.title}</span>
             </div>
+            <p>{item.text}</p>
           </div>
         ))}
       </div>
     </section>
   );
 }
-
-
