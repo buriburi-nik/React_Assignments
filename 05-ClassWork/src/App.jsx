d2941fa8683542afdd7614062baf2336823602a9
buriburi-nik <niikgharat001@gmail.com>
2025-02-20 22:22:41 +0530

    Implement subject management with add, remove, and hour adjustment features

--- 05-ClassWork/src/App.jsx
+++ 05-ClassWork/src/App.jsx
@@ -1,34 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [subjectName, setSubjectName] = useState('');
+  const [hours, setHours] = useState('');
+  const [subjects, setSubjects] = useState([]);
+
+  useEffect(() => {
+    const storedSubjects = localStorage.getItem('geetksterSubjects');
+    if (storedSubjects) {
+      setSubjects(JSON.parse(storedSubjects));
+    }
+  }, [])
+
+  useEffect(() => { 
+    localStorage.setItem('geetksterSubjects', JSON.stringify(subjects));
+  }, [subjects])
+
+  const handleAddSubject = () => { 
+    if (!subjectName.trim()) return alert('Please enter a subject'); 
+    const newSubject = {
+      id: subjects.length + 1,
+      name: subjectName,
+      hours: parseInt(hours),
+    };
+
+    setSubjects([...subjects, newSubject]);
+    setSubjectName('');
+    setHours('');
+  }
+
+  const handleRemoveSubject = (id) => {
+    setSubjects(subjects.filter((subject) => subject.id !== id));
+
+   }
+
+  const increamentHours = (hours) => {
+    setSubjects(subjects.map((subject) => subject.id === hours ? { ...subject, hours: subject.hours + 1 } : subject));
+    setHours('');
+    setSubjectName('');
+
+  }
+
+  const decreaseHours = (hours) => {
+    setSubjects(subjects.map((subject) => subject.id === hours ? { ...subject, hours: Math.max(0, subject.hours - 1) } : subject));
+    setHours('');
+    setSubjectName('');
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h1>Geekster Eduction Planner</h1>
+
+      
+      <div className="input-cont">
+        <input
+          type="text"
+          placeholder='Subject-Name'
+          value={subjectName}
+          className='input-field'
+          onChange={(e) =>setSubjectName(e.target.value)}
+        />
+
+        < input
+          type="text"
+          placeholder='hours'
+          className='input-field'
+          value={hours}
+          onChange={(e) => setHours(e.target.value)}
+        />
+
+        <button
+          onClick={handleAddSubject} className='add-btn'        
+        >Add</button>  
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <ul className="subject-list">
+        {subjects.map((subject) => (
+          <li key={subject.id} className='list-item'>   
+            <span className='subject-name'>{subject.name} </span>
+            <span className='subject-hours'>{subject.hours} hours</span>
+            <button
+              onClick={() => handleRemoveSubject(subject.id)}
+              className='remove-btn'
+            >Remove</button>
+
+            <button
+              onClick={() => increamentHours(subject.id)}
+              className='btn'
+            >+</button>
+            
+            <button
+              onClick={() => decreaseHours(subject.id)}
+              className='btn'
+            >-</button>
+          </li>
+        ))}
+      </ul>
+      
+    </div>
+    
   )
 }
 
